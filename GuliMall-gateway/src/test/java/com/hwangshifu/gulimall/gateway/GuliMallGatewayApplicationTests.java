c68c377f2a14d988ded83151036407b3dd65f5cc
sanqi <hwangshifuu@gmail.com>
2022-06-25 19:24:01 +0800

    引入Spring Cloud Gateway网关

---
+++ GuliMall-gateway/src/test/java/com/hwangshifu/gulimall/gateway/GuliMallGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hwangshifu.gulimall.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuliMallGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
