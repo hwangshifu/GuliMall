e961f3ce2aa8a3f630be494eb80f21089dd7d645
sanqi <hwangshifuu@gmail.com>
2022-07-01 17:31:42 +0800

    跨域解决 新增路由规则

--- GuliMall-gateway/src/main/java/com/hwangshifu/gulimall/gateway/GuliMallGatewayApplication.java
+++ GuliMall-gateway/src/main/java/com/hwangshifu/gulimall/gateway/GuliMallGatewayApplication.java
@@ -10,7 +10,6 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @EnableDiscoveryClient
 @SpringBootApplication
 public class GuliMallGatewayApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(GuliMallGatewayApplication.class, args);
 	}
---
+++ GuliMall-gateway/src/main/java/com/hwangshifu/gulimall/gateway/config/GuliMallCorsConfiguration.java
@@ -0,0 +1,31 @@
+package com.hwangshifu.gulimall.gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+import java.util.Collections;
+
+/**
+ * @author SANQI
+ * 跨域配置
+ */
+@Configuration
+public class GuliMallCorsConfiguration {
+
+    @Bean
+    public CorsWebFilter corsWebFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        //1、配置跨域
+        corsConfiguration.setAllowedMethods(Collections.singletonList("*"));
+        corsConfiguration.setAllowedHeaders(Collections.singletonList("*"));
+        corsConfiguration.setAllowedOriginPatterns(Collections.singletonList("*"));
+        corsConfiguration.setAllowCredentials(true);
+        source.registerCorsConfiguration("/**", corsConfiguration);
+        return new CorsWebFilter(source);
+    }
+}
